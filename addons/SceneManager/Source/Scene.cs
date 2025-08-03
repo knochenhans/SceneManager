2d8dff0b16da077ce388fd56eee3485785b17a08
Andre Jonas <nipsky@googlemail.com>
2025-08-03 02:15:42 +0200

    Add LifeTimer

--- addons/SceneManager/Source/Scene.cs
+++ addons/SceneManager/Source/Scene.cs
@@ -1,36 +1,30 @@
 using Godot;
-using System.Threading.Tasks;
 using static Logger;
 
 public partial class Scene : Node
 {
-	// [Signal] public delegate void ReadyFinishedEventHandler();
-	// [Signal] public delegate void ExitFinishedEventHandler();
-
 	[Export] public float FadeInTime = 0.5f;
 	[Export] public float FadeOutTime = 0.5f;
+	[Export] public float LifeTime = 0.0f;
 	[Export] public string DefaultNextScene = "";
 
-	// PackedScene FadeScene = ResourceLoader.Load<PackedScene>("res://addons/SceneManager/Nodes/Fade.tscn");
 	CanvasLayer CanvasLayerNode => GetNode<CanvasLayer>("CanvasLayer");
 	protected ColorRect BackgroundNode => GetNode<ColorRect>("ColorRect");
-
-	// private async Task Fade(Fade.FadeDirectionEnum direction, float time)
-	// {
-	// 	var fade = FadeScene.Instantiate<Fade>();
-	// 	CanvasLayerNode.AddChild(fade);
-	// 	fade.Run(direction, time);
-	// 	await ToSignal(fade, global::Fade.SignalName.FadeFinished);
-	// }
+	Timer LifeTimerNode => GetNode<Timer>("LifeTimer");
 
 	public override void _Ready()
 	{
 		BackgroundNode.GuiInput += OnBackgroundClicked;
 
-		Log($"Starting scene {SceneFilePath}", "SceneManager", LogTypeEnum.Framework);
-		// await Fade(global::Fade.FadeDirectionEnum.In, FadeInTime);
+		if (LifeTime > 0)
+		{
+			LifeTimerNode.WaitTime = LifeTime;
+			LifeTimerNode.Start();
+			LifeTimerNode.Timeout += ChangeToNextScene;
+			Log($"Scene {Name} will change to next scene after {LifeTime} seconds.", "Scene", LogTypeEnum.Framework);
+		}
 
-		// EmitSignal(SignalName.ReadyFinished);
+		Log($"Starting scene {SceneFilePath}", "SceneManager", LogTypeEnum.Framework);
 	}
 
 	protected virtual void OnBackgroundClicked(InputEvent @event)
@@ -39,8 +33,5 @@ public partial class Scene : Node
 			ChangeToNextScene();
 	}
 
-	protected async void ChangeToNextScene()
-	{
-		await SceneManager.Instance.ChangeToDefaultNextScene();
-	}
+    protected async void ChangeToNextScene() => await SceneManager.Instance.ChangeToDefaultNextScene();
 }
