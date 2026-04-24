4ef2835e704d9ff89de6fd8be5b70e11d1ea6487
Andre Jonas <nipsky@googlemail.com>
2026-04-25 01:29:19 +0200

    Refactor HUD initialization to include StatDefinitionDatabase parameter

--- addons/SceneManager/Nodes/HUD.cs
+++ addons/SceneManager/Nodes/HUD.cs
@@ -7,29 +7,19 @@ public partial class HUD : CanvasLayer
     protected Control HUDMarginContainer => GetNode<Control>("%HUDMarginContainer");
     protected GameContext GameContext;
     protected Node2D Player;
+    protected StatDefinitionDatabase StatDefinitionDatabase;
     #endregion
 
     #region [Lifecycle]
-    public virtual void Init(GameContext gameContext, Node2D player = null)
+    public virtual void Init(GameContext gameContext, Node2D player = null, StatDefinitionDatabase statDefinitionDatabase = null)
     {
         GameContext = gameContext;
         Player = player;
+        StatDefinitionDatabase = statDefinitionDatabase;
     }
 
     public virtual void Uninit()
     {
     }
     #endregion
-
-    // #region [Public]
-    // public void EnableUIInput()
-    // {
-    //     HUDMarginContainer.MouseFilter = Control.MouseFilterEnum.Stop;
-    // }
-
-    // public void DisableUIInput()
-    // {
-    //     HUDMarginContainer.MouseFilter = Control.MouseFilterEnum.Ignore;
-    // }
-    // #endregion
 }
