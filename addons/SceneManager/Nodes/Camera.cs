3d1c15fbd88a6e46fe5f959c5f84b8c813e3feba
Andre Jonas <nipsky@googlemail.com>
2026-09-12 22:44:36 +0200

    Working on camera

--- addons/SceneManager/Nodes/Camera.cs
+++ addons/SceneManager/Nodes/Camera.cs
@@ -5,8 +5,11 @@ public partial class Camera : Camera2D, ICamera<Vector2, Node2D>
 {
     #region [Fields and Properties]
     [ExportGroup("Limit Settings")]
-    [Export] public bool LimitToStageLimits = false;
+    [Export] public bool LimitToStageLimits = true; // Enabled by default
     [Export] public Vector2 StageLimitAddedMargin = new(16, 16);
+
+    private Rect2 currentStageLimits;
+    private bool hasLimits = false;
 
     [ExportGroup("Shake Settings")]
     [Export] float ShakeDecay = 3f;
@@ -21,18 +24,19 @@ public partial class Camera : Camera2D, ICamera<Vector2, Node2D>
     public Node2D FollowTarget;
     #endregion
 
+    public override void _Ready() => AnchorMode = AnchorModeEnum.DragCenter;
+
     public void SetFollowTarget(Node2D target) => FollowTarget = target;
     public void ClearFollowTarget() => FollowTarget = null;
+    public void SetFollowTarget(Node target) => FollowTarget = target as Node2D;
 
     public void SetLimits(Rect2 stageLimits)
     {
-        if (!LimitToStageLimits)
-            return;
+        currentStageLimits = stageLimits;
+        hasLimits = true;
 
-        LimitLeft = (int)stageLimits.Position.X - (int)StageLimitAddedMargin.X;
-        LimitTop = (int)stageLimits.Position.Y - (int)StageLimitAddedMargin.Y;
-        LimitRight = (int)stageLimits.End.X + (int)StageLimitAddedMargin.X;
-        LimitBottom = (int)stageLimits.End.Y + (int)StageLimitAddedMargin.Y;
+        if (LimitToStageLimits)
+            ClampPositionToLimits();
     }
 
     public void Reset()
@@ -45,15 +49,24 @@ public partial class Camera : Camera2D, ICamera<Vector2, Node2D>
 
     public async Task MoveTo(Vector2 position, float duration, Tween.TransitionType transitionType = Tween.TransitionType.Linear, Tween.EaseType easeType = Tween.EaseType.InOut)
     {
+        Vector2 targetPos = (hasLimits && LimitToStageLimits) ? GetClampedPosition(position) : position;
+
         var tcs = new TaskCompletionSource();
         var tween = CreateTween();
         tween.SetEase(easeType);
         tween.SetTrans(transitionType);
-        tween.TweenProperty(this, "position", position, duration);
+
+        tween.TweenProperty(this, "position", targetPos, duration);
+
+        if (hasLimits && LimitToStageLimits)
+        {
+            tween.TweenCallback(Callable.From(() => Position = GetClampedPosition(Position)));
+        }
+
         tween.Finished += tcs.SetResult;
         await tcs.Task;
 
-        Logger.Log($"Camera moved to {position}", Logger.LogTypeEnum.World);
+        Logger.Log($"Camera moved to {Position}", Logger.LogTypeEnum.World);
     }
 
     public async Task ZoomTo(Vector2 zoom, float duration, Tween.TransitionType transitionType = Tween.TransitionType.Linear, Tween.EaseType easeType = Tween.EaseType.InOut)
@@ -71,9 +84,42 @@ public partial class Camera : Camera2D, ICamera<Vector2, Node2D>
 
     public override void _Process(double delta)
     {
-        if (FollowTarget != null)
+        if (FollowTarget != null && IsInstanceValid(FollowTarget))
             Position = Position.Lerp(FollowTarget.Position, 0.1f);
+
+        if (hasLimits && LimitToStageLimits)
+            ClampPositionToLimits();
     }
+
+    public Vector2 GetClampedPosition(Vector2 targetPosition)
+    {
+        if (!hasLimits || !LimitToStageLimits)
+            return targetPosition;
+
+        Vector2 viewportSizeWorld = GetViewportRect().Size / Zoom;
+        Vector2 halfViewSize = viewportSizeWorld / 2.0f;
+
+        float minX = currentStageLimits.Position.X - StageLimitAddedMargin.X + halfViewSize.X;
+        float maxX = currentStageLimits.End.X + StageLimitAddedMargin.X - halfViewSize.X;
+        float minY = currentStageLimits.Position.Y - StageLimitAddedMargin.Y + halfViewSize.Y;
+        float maxY = currentStageLimits.End.Y + StageLimitAddedMargin.Y - halfViewSize.Y;
+
+        Vector2 clampedPos = targetPosition;
+
+        if (minX > maxX)
+            clampedPos.X = currentStageLimits.Position.X + (currentStageLimits.Size.X / 2.0f);
+        else
+            clampedPos.X = Mathf.Clamp(targetPosition.X, minX, maxX);
+
+        if (minY > maxY)
+            clampedPos.Y = currentStageLimits.Position.Y + (currentStageLimits.Size.Y / 2.0f);
+        else
+            clampedPos.Y = Mathf.Clamp(targetPosition.Y, minY, maxY);
+
+        return clampedPos;
+    }
+
+    private void ClampPositionToLimits() => Position = GetClampedPosition(Position);
 
     public void Enable(bool enable)
     {
@@ -84,6 +130,4 @@ public partial class Camera : Camera2D, ICamera<Vector2, Node2D>
         SetProcessInput(enable);
         SetPhysicsProcess(enable);
     }
-
-    public void SetFollowTarget(Node target) => FollowTarget = target as Node2D;
 }
